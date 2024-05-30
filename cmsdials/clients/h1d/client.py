59d552250d180d8c150862354013fddddf6e0e38
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-05-30 12:08:04 +0200

    refactor: update h1d client and base model
    - Remove hist_id attribute from base model;
    - Add get method to h1d client with multiple args, since th1d table is
    using a composite primary key and the base get method from
    BaseAuthorizedAPIClient only accepts one argument for the get method.

--- cmsdials/clients/h1d/client.py
+++ cmsdials/clients/h1d/client.py
@@ -1,4 +1,8 @@
+import requests
+from requests.exceptions import HTTPError
+
 from ...utils.api_client import BaseAuthorizedAPIClient
+from ...utils.logger import logger
 from .models import LumisectionHistogram1D, LumisectionHistogram1DFilters, PaginatedLumisectionHistogram1DList
 
 
@@ -7,3 +11,17 @@ class LumisectionHistogram1DClient(BaseAuthorizedAPIClient):
     pagination_model = PaginatedLumisectionHistogram1DList
     filter_class = LumisectionHistogram1DFilters
     lookup_url = "th1/"
+
+    def get(self, dataset_id: int, run_number: int, ls_number: int, me_id: int):
+        endpoint_url = f"{self.api_url}{self.lookup_url}{dataset_id}/{run_number}/{ls_number}/{me_id}/"
+        headers = self._build_headers()
+        response = requests.get(endpoint_url, headers=headers, timeout=self.default_timeout)
+
+        try:
+            response.raise_for_status()
+        except HTTPError as err:
+            logger.info(f"Api raw response: {response.text}")
+            raise err
+
+        response = response.json()
+        return self.data_model(**response)
