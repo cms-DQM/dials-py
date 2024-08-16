09cef0052d70c94e824e3797426dfe17eb743e0f
Yueh-Shun Li <shamrocklee@posteo.net>
2024-08-17 02:06:58 +0800

    tests: specify retries

--- tests/integration/test_h2d_client.py
+++ tests/integration/test_h2d_client.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from urllib3.util import Retry
 
 from cmsdials.clients.h2d.models import LumisectionHistogram2D, PaginatedLumisectionHistogram2DList
 from cmsdials.filters import LumisectionHistogram2DFilters
@@ -14,13 +15,13 @@ def test_get_h2d() -> None:
 
 def test_list_h2d() -> None:
     dials = setup_dials_object()
-    data = dials.h2d.list()
+    data = dials.h2d.list(retries=Retry(total=3, backoff_factor=0.1))
     assert isinstance(data, PaginatedLumisectionHistogram2DList)
     assert isinstance(data.to_pandas(), pd.DataFrame)
 
 
 def test_list_all_h2d() -> None:
     dials = setup_dials_object()
-    data = dials.h2d.list_all(LumisectionHistogram2DFilters(), max_pages=5)
+    data = dials.h2d.list_all(LumisectionHistogram2DFilters(), max_pages=5, retries=Retry(total=5, backoff_factor=0.1))
     assert isinstance(data, PaginatedLumisectionHistogram2DList)
     assert isinstance(data.to_pandas(), pd.DataFrame)
