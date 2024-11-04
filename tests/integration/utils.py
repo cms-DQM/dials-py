9ee50307e491037cf7b6b20a4cbfb44d0408fe15
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-11-04 10:54:03 +0100

    refactor: update `setup_dials_object` in tests to optionally specify workspace

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -1,11 +1,12 @@
 import os
+from typing import Optional
 
 from cmsdials import Dials
 from cmsdials.auth.secret_key import Credentials
 
 
-def setup_dials_object() -> Dials:
+def setup_dials_object(workspace: Optional[str] = None) -> Dials:
     secret_key = os.getenv("SECRET_KEY")
     base_url = os.getenv("BASE_URL")
     creds = Credentials(token=secret_key)
-    return Dials(creds, base_url=base_url)
+    return Dials(creds, base_url=base_url, workspace=workspace)
